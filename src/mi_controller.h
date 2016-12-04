4c4f6d22e8561e58d3021a3c5c3c4d23f82afc8e
Pazderski <Pazderski@DPHome>
2016-12-04 21:36:28 +0100

    Few slight changes in the source. Add missing files!

--- src/mi_controller.h
+++ src/mi_controller.h
@@ -34,16 +34,16 @@ public:
 		return &cmd;
 	}
 
-	SdoCmd * MapRPDO(int CmdCode)
+	SdoCmd * MapRPDO(int cmdCode)
 	{
 		cmd.type = 0x23;
 		cmd.index = 0x1600;
 		cmd.subindex = 0x01;
 		*(uint32_t*)cmd.data = 0;
-		cmd.data[0] = CmdCode;
-		cmd.data[1] = CmdCode >> 8;
-		cmd.data[2] = CmdCode >> 16;
-		cmd.data[3] = CmdCode >> 24;
+		cmd.data[0] = cmdCode;
+		cmd.data[1] = cmdCode >> 8;
+		cmd.data[2] = cmdCode >> 16;
+		cmd.data[3] = cmdCode >> 24;
 		cmd.timeout = 10;
 		cmd.trials = -1;
 		return &cmd;
@@ -131,15 +131,17 @@ public:
 	}
 
 
-	SdoCmd * SetSubvel(int Value)
+	SdoCmd * SetSubvel(int32_t value)
 	{
 		cmd.type = 0x23;
 		cmd.index = 0x3500;
 		cmd.subindex = 0x00;
-		cmd.data[0] = Value;
-		cmd.data[1] = Value >> 8;
-		cmd.data[2] = Value >> 16;
-		cmd.data[3] = Value >> 24;
+
+		// Is it possible to use something like this: "*(uint32_t*)cmd.data = value" ?
+		cmd.data[0] = value;
+		cmd.data[1] = value >> 8;
+		cmd.data[2] = value >> 16;
+		cmd.data[3] = value >> 24;
 		cmd.timeout = 5;
 		cmd.trials = -1;
 		return &cmd;
