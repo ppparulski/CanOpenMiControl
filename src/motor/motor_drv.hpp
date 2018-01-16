8ac5bc054a96c8f7ed4b34a6822423c0723e6735
Tomasz Krzykowski <krzykowski.tomek@gmail.com>
2018-01-16 16:42:46 +0100

    Working code for double TPDO mapping

--- src/motor/motor_drv.hpp
+++ src/motor/motor_drv.hpp
@@ -43,19 +43,37 @@ public:
 		sdo.PushCommand(MiControlCmds::MapRPDO(1, MiControlCmds::SetSubvel(0), MiControlCmds::DataSize32)); // Alternatywna sk³adnia: MapRPDO(1, 0x3500, 0, 32)
 		sdo.PushCommand(MiControlCmds::EnableRPDO(1));
 
-		// TPDO 0
+
+
+		//Map position and velocity to PDO 181
+		sdo.PushCommand(MiControlCmds::DisableTPDO(0));
+		sdo.PushCommand(MiControlCmds::SetTransmissionType(0));
+		sdo.PushCommand(MiControlCmds::Map02hTxPDO(0, 0x3762, 0, MiControlCmds::DataSize32)); //position
+		sdo.PushCommand(MiControlCmds::Map02hTxPDO(1, 0x3A04, 1, MiControlCmds::DataSize32)); //velocity
+		sdo.PushCommand(MiControlCmds::EnableTPDO(1, 2)); //pdo 1 = (-1) + 0, 2 objects mapped
+
+		//Map current and status to PDO 281
+		sdo.PushCommand(MiControlCmds::DisableTPDO(1));
+		sdo.PushCommand(MiControlCmds::SetTransmissionType(1));
+		sdo.PushCommand(MiControlCmds::Map02hTxPDO(0, 0x3262, 0, MiControlCmds::DataSize32)); //current
+		sdo.PushCommand(MiControlCmds::Map02hTxPDO(1, 0x3002, 1, MiControlCmds::DataSize32)); //
+		sdo.PushCommand(MiControlCmds::EnableTPDO(2, 2));
+
+
+
+		// TPDO 0  //  181
 		//sdo.PushCommand(MiControlCmds::DisableTPDO(0));
 		//sdo.PushCommand(MiControlCmds::SetTransmissionType(0));
 		//sdo.PushCommand(MiControlCmds::MapTPDO(0, 0x3762, 0, MiControlCmds::DataSize32)); // Pozycja enkodera (liczba impulsów).
 		//sdo.PushCommand(MiControlCmds::EnableTPDO(1, 1));
 
-		// TPDO 1
+		// TPDO 1  //  281
 		//sdo.PushCommand(MiControlCmds::DisableTPDO(1));
 		//sdo.PushCommand(MiControlCmds::SetTransmissionType(1));
 		//sdo.PushCommand(MiControlCmds::MapTPDO(1, 0x3A04, 1, MiControlCmds::DataSize32)); // Prêdkosc enkodera.
 		//sdo.PushCommand(MiControlCmds::EnableTPDO(2, 1));
 
-		// TPDO 2
+		// TPDO 2  //  381
 		//sdo.PushCommand(MiControlCmds::DisableTPDO(2));
 		//sdo.PushCommand(MiControlCmds::SetTransmissionType(2));
 		//sdo.PushCommand(MiControlCmds::MapTPDO(2, 0x3262, 0, MiControlCmds::DataSize32)); // Pr¹d silnika.
